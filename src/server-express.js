d6ea566cba10488356a3e285e360ce359dbe877f
Rebeca Zaia <rebeca.zaia@estudante.ifms.edu.br>
2025-09-12 14:20:14 -0400

    Update server-express.js

--- src/server-express.js
+++ src/server-express.js
@@ -1,1 +1,20 @@
 //no terminal: npm i express
+import express from 'express'
+const app = express()
+
+app.use(espress.json())
+
+const usuarios = []
+app.get('/user', (req, res)=>{
+  res.status(200).json(usuarios)
+})
+
+app.post('/user', (req, res) => {
+  const {nome, idade} = req.body;
+  usuarios.push({nome, idade})
+  res.status(201).send("Tudo ok")
+})
+
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333")
+})
