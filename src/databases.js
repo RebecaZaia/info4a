531fe470532d8b154827b192cf8b18773cc799b7
Rebeca Zaia <rebeca.zaia@estudante.ifms.edu.br>
2025-10-24 14:16:58 -0400

    Update databases.js

--- src/databases.js
+++ src/databases.js
@@ -1,3 +1,7 @@
+import {error} from 'node:console'
+import fs from 'node:fs/promises'
+
+const databasePath = new URL
 
 
 
@@ -6,10 +10,19 @@
 
 
 
+    .catch(() => {
+      this.#persist()
+    })
+}
 
+#persist(){
+  fs.writeFile(databasePath, JSON.stringify(this.#database))
+}
 
-
-
+select(table){
+  const data = this.#database[table] ?? []
+  return data
+}
 
 selectItem (table, id){
   const data = this.#database[table] ?? []
@@ -18,4 +31,40 @@ selectItem (table, id){
     item = data.find(item => item.id == id)
   }
   return item
+}
+
+delete(table, id){
+  try{
+    const data = this.#database[table] ?? []
+    const newData = data.filter(item => item.id != id)
+    this.#database[table] = [data]
+    this.#persist()
+    return{
+      error: false,
+      mensagem:"Sucesso ao deletar"
+    }
+  }catch(error){
+    return ({
+      error: error,
+      mensagem: "Falha ao deletar"
+    })
+  }
+}
+
+update(table, id, novo){
+  const data = this.#database[table] ?? []
+  const indice = data.findIndex(item => item.id == id)
+  if(indice != -1){
+    data.splice(indice, 1, novo)
+    this.#persist()
+    return{
+      error: false,
+      mensagem: "Alterado com sucesso"
+    }
+
+    return{
+      error:true,
+      mensagem:"Id não encontrado"
+    }
+  }
 }
