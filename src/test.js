b3f7892a9ff957838af68700dc67e816c1ff8db0
Rebeca Zaia <rebeca.zaia@estudante.ifms.edu.br>
2025-10-24 14:22:10 -0400

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,1 +1,12 @@
+const banco = {
+    usuario: [
+        {nome: "Pedro", idade : 17},
+        {nome: "Henrique", idade: 18}
+    ],
+    produtos: [
+        {nome: "Mouse", valor : 150},
+        {nome: "Teclado", valor : 150}
+    ]
+}
 
+console.log(Array.isArray(banco["usuario2"]))
